e5c4fbe3f4c28a8cb31ced45a87523f72dd5b1ea
boe1999 <boerio99@gmail.com>
2025-08-13 17:57:32 +0200

    inizializzate le migrazioni del db

--- backend/prisma/migrations/20250813155511_13_08_2025_login/migration.sql
+++ backend/prisma/migrations/20250813155511_13_08_2025_login/migration.sql
@@ -2,7 +2,8 @@
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
-    "password" TEXT NOT NULL
+    "password" TEXT NOT NULL,
+    "participant" BOOLEAN NOT NULL DEFAULT false
 );
 
 -- CreateIndex
